9ff53033c5454bc5c60d7adca965e63ca44803da
Mike Campbell <exactmike@users.noreply.github.com>
2023-02-04 19:43:15 -0500

    updated Get-OGReadableSKU

--- OGraph/functions/Get-OGReadableSku.ps1
+++ OGraph/functions/Get-OGReadableSku.ps1
@@ -11,7 +11,8 @@ Get-OGReadableSku
 .NOTES
 
 #>
-function Get-OGReadableSku {
+function Get-OGReadableSku
+{
     [CmdletBinding()]
     param (
         [switch]$StoreCSV
@@ -22,27 +23,42 @@ function Get-OGReadableSku {
     {
         $True
         {
-            Import-Csv $PreDownloadedCSV
+            switch ($StoreCSV)
+            {
+                $true
+                {
+
+                }
+                $False
+                {
+                    Import-Csv $PreDownloadedCSV
+                }
+            }
         }
         $False
         {
-            try {
-                Invoke-WebRequest -Uri $url -OutFile $joinPath
-                $url = "https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv"
-                $temp = Get-PSDrive -Name "Temp"
-                $TempPath = Join-Path -Path $temp.Root -ChildPath "OGReadableSku.csv"
-                Import-CSV $TempPath
+            try
+            {
+                $temp = Get-PSDrive -Name 'Temp'
+                $TempPath = Join-Path -Path $temp.Root -ChildPath 'OGReadableSku.csv'
+                $url = 'https://download.microsoft.com/download/e/3/e/e3e9faf2-f28b-490a-9ada-c6089a1fc5b0/Product%20names%20and%20service%20plan%20identifiers%20for%20licensing.csv'
+                Invoke-WebRequest -Uri $url -OutFile $TempPath
+                switch ($StoreCSV)
+                {
+                    $True
+                    {
+                        Move-Item -Path $TempPath -Destination $PreDownloadedCSV -Force -Confirm:$false
+                    }
+                    $False
+                    {
+                        Import-Csv $TempPath
+                    }
+                }
             }
-            catch {
+            catch
+            {
                 Out-Null
             }
-        }
-    }
-    switch ($StoreCSV)
-    {
-        $True
-        {
-            Move-Item -Path $TempPath -Destination $PreDownloadedCSV -Force -Confirm:$false
         }
     }
 }

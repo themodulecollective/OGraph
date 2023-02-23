e437934486c614e5d7d60b7ac9759b7af16db548
Mike Campbell <exactmike@users.noreply.github.com>
2023-02-23 16:00:56 -0500

    Help and Formatting updates

--- OGraph/functions/Get-OGGroupLicense.ps1
+++ OGraph/functions/Get-OGGroupLicense.ps1
@@ -8,7 +8,7 @@ Gets the license skus applied by a group and any disabled service plans of those
 Permissions: https://learn.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0&tabs=http
 
 .PARAMETER GroupId
-Parameter description
+GroupID (guid) for the group
 
 .EXAMPLE
 Get-OGGroupLicense -GroupId f6557fc2-d4a5-4266-8f4c-2bdcd0cd9a2d
@@ -16,13 +16,13 @@ Get-OGGroupLicense -GroupId f6557fc2-d4a5-4266-8f4c-2bdcd0cd9a2d
 .NOTES
 General notes
 #>
-Function Get-OGGroupLicense {
-
+Function Get-OGGroupLicense
+{
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]$GroupId
     )
+
     $URI = "/$GraphVersion/groups/$GroupId/assignedLicenses"
     Get-OGNextPage -uri $URI
-
 }

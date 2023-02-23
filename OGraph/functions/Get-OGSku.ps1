234205aeca2d01c8175f3105e24220e09d8e7079
Mike Campbell <exactmike@users.noreply.github.com>
2023-02-23 16:03:16 -0500

    add IncludeDisplayName and ServicePlanDelimiter paramaters to Get-OGSku

--- OGraph/functions/Get-OGSku.ps1
+++ OGraph/functions/Get-OGSku.ps1
@@ -1,18 +1,56 @@
 <#
 .SYNOPSIS
-Get Azure AD Skus consumed in the tenant
+Get Skus available in the connected Microsoft tenant
 
 .DESCRIPTION
-Long description
+Gets Skus available in the connected Microsoft Tenant and allows the inclusion of "human readable" DisplayNames.
 
-Permissions: https://learn.microsoft.com/en-us/graph/api/subscribedsku-list?view=graph-rest-1.0&tabs=http
+.PARAMETER IncludeDisplayName
+Includes "human readable" display names in the output for both skus and serviceplans.
+
+.PARAMETER ServicePlanDelimiter
+Specify the delimiter for the ServicePlanNames and ServicePlanDisplayNames.  Defaults to ";".
+
 .EXAMPLE
 Get-OGSku
 
 .NOTES
-General notes
+Permissions Required: https://learn.microsoft.com/en-us/graph/api/subscribedsku-list?view=graph-rest-1.0&tabs=http
 #>
-Function Get-OGSku {
+Function Get-OGSku
+{
+    [CmdletBinding()]
+    param(
+        [switch]$IncludeDisplayName
+        ,
+        [parameter()]
+        [ValidateLength(1,1)]
+        [string]$ServicePlanDelimiter = ';'
+    )
     $Uri = "/$GraphVersion/subscribedSkus"
-    get-ognextpage -uri $Uri
+    switch ($IncludeDisplayName)
+    {
+        $true
+        {
+            $rawSku = get-ognextpage -uri $Uri
+            $ReadableHash = @{}
+            $skusReadable = Get-OGReadableSku
+            foreach ($sR in $skusReadable)
+            {
+                $ReadableHash[$sR.GUID] = $sR.Product_Display_Name
+                $ReadableHash[$sR.Service_Plan_Id] = $sR.Service_Plans_Included_Friendly_Names
+            }
+            foreach ($s in $rawSku)
+            {
+                $s | Select-Object -Property *,
+                @{n='skuDisplayName';e = {$ReadableHash[$s.skuid]}},
+                @{n='servicePlanNames';e= {$s.ServicePlans.foreach({$_.ServicePlanName}) -join $ServicePlanDelimiter}},
+                @{n='servicePlanDisplayNames'; e= {$s.ServicePlans.foreach({$ReadableHash[$_.ServicePlanID]}) -join $ServicePlanDelimiter}}
+            }
+        }
+        $false
+        {
+            get-ognextpage -uri $Uri
+        }
+    }
 }

a2d3a70051eea146139bf7e0a719f44148eb47bf
Mike Campbell <exactmike@users.noreply.github.com>
2023-02-23 16:02:30 -0500

    breaking change - require -IncludeDisplayName for DisplayName inclusion rather than by default

--- OGraph/functions/Get-OGGroupLicenseReport.ps1
+++ OGraph/functions/Get-OGGroupLicenseReport.ps1
@@ -1,17 +1,20 @@
 <#
 .SYNOPSIS
-Report all Enabled and Disabled service plans applied by a group
+Report all Enabled and Disabled skus and service plans applied by a group
 
 .DESCRIPTION
-Report all Enabled and Disabled service plans applied by a group
+Report all Enabled and Disabled skus and service plans applied by a group
 
 Permissions: https://learn.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0&tabs=http
 
 .PARAMETER GroupId
-Parameter description
+GroupID (guid) for the group for which to report licensing details.
 
 .PARAMETER All
-Parameter description
+Includes all groups that have a license assignment.
+
+.PARAMETER IncludeDisplayName
+Includes "human readable" Display Names for skus and service plans.
 
 .EXAMPLE
 Get-OGGroupLicenseReport -GroupId f6557fc2-d4a5-4266-8f4c-2bdcd0cd9a2d
@@ -24,32 +27,43 @@ Function Get-OGGroupLicenseReport
 
     [CmdletBinding(DefaultParameterSetName = 'Single')]
     param (
-        [Parameter(Mandatory, ParameterSetName = 'Single')]$GroupId,
-        [Parameter(Mandatory, ParameterSetName = 'All')][switch]$All
+        [Parameter(Mandatory, ParameterSetName = 'Single')]
+        $GroupId,
+        [Parameter(Mandatory, ParameterSetName = 'All')]
+        [switch]$All,
+        [Parameter()]
+        [switch]$IncludeDisplayName
     )
     switch ($PSCmdlet.ParameterSetName)
     {
 
         'All'
         {
-            $groups = Get-OGGroup -all -property assignedLicenses | Where-Object assignedLicenses -NE $null
-            $groups.foreach({ Get-OGGroupLicenseReport -groupid $_.id })
+            $groups = @(Get-OGGroup -All -Property assignedLicenses).where({$_.assignedLicenses.count -ge 1})
+            $getOGGLRParams = @{IncludeDisplayName = $IncludeDisplayName}
+            $groups.foreach({ Get-OGGroupLicenseReport -GroupId $_.id @getOGGLRParams})
         }
 
         'Single'
         {
-            $skus = Get-OGSku
-            $skusReadable = Get-OGReadableSku
-            $group = Get-OGGroup -groupid $groupid
             $ReadableHash = @{}
+            switch ($IncludeDisplayName)
+            {
+                $true
+                {
+                    $skusReadable = Get-OGReadableSku
+                    foreach ($sR in $skusReadable)
+                    {
+                        $ReadableHash[$sR.GUID] = $sR.Product_Display_Name
+                        $ReadableHash[$sR.Service_Plan_Id] = $sR.Service_Plans_Included_Friendly_Names
+                    }
+                }
+            }
+            $skus = Get-OGSku
+            $group = Get-OGGroup -GroupId $groupid
             $skuHash = @{}
             $spHash = @{}
             $spPerSku = @{}
-            foreach ($sR in $skusReadable)
-            {
-                $ReadableHash[$sR.GUID] = $sR.Product_Display_Name
-                $ReadableHash[$sR.Service_Plan_Id] = $sR.Service_Plans_Included_Friendly_Names
-            }
             foreach ($s in $skus)
             {
                 $sku = [PSCustomObject]@{

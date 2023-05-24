34bfc28b8ddec426bf62c9f314af4a8311bae9f1
Mike Campbell <exactmike@users.noreply.github.com>
2023-05-24 16:10:54 -0400

    Get-OGUserSku Add skuDisplayName, ServicePlanDisplayName, and UserPrincipalName Passthru

--- OGraph/functions/Get-OGUserSku.ps1
+++ OGraph/functions/Get-OGUserSku.ps1
@@ -19,8 +19,42 @@ Function Get-OGUserSku {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]$UserPrincipalName
+        ,
+        [switch]$IncludeDisplayName
+        ,
+        [parameter()]
+        [ValidateLength(1,1)]
+        [string]$ServicePlanDelimiter = ';'
+        ,
+        [parameter()]
+        [alias('PassthruUPN')]
+        [switch]$PassthruUserPrincipalName
     )
 
+    $ReadableHash = @{}
+    switch ($IncludeDisplayName)
+    {
+        $true
+        {
+            $skusReadable = Get-OGReadableSku
+            foreach ($sR in $skusReadable)
+            {
+                $ReadableHash[$sR.GUID] = $sR.Product_Display_Name
+                $ReadableHash[$sR.Service_Plan_Id] = $sR.Service_Plans_Included_Friendly_Names
+            }
+        }
+    }
+
     $Uri = "$GraphVersion/Users/$($UserPrincipalName)/licenseDetails"
-    Get-OGNextPage -uri $Uri
+    $rawSku = Get-OGNextPage -uri $Uri
+    if ($PassthruUserPrincipalName) {
+        $rawSku = $rawSku | Select-Object -Property @{n='UserPrincipalName';e={$UserPrincipalName}},*
+    }
+    foreach ($s in $rawSku)
+    {
+        $s | Select-Object -Property *,
+        @{n='skuDisplayName';e = {$ReadableHash[$s.skuid]}},
+        @{n='servicePlanNames';e= {$s.ServicePlans.foreach({$_.ServicePlanName}) -join $ServicePlanDelimiter}},
+        @{n='servicePlanDisplayNames'; e= {$s.ServicePlans.foreach({$ReadableHash[$_.ServicePlanID]}).where({$null -ne $_}) -join $ServicePlanDelimiter}}
+    }
 }

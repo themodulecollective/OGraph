82ded5c4c0c0cb550e434c9f5d9a1d1f7600492c
clouddots <ben@clouddots.io>
2024-09-18 12:32:14 -0400

    Column Selection Added

--- OGraph/functions/Get-OGSiteListItem.ps1
+++ OGraph/functions/Get-OGSiteListItem.ps1
@@ -15,6 +15,9 @@ SharePoint List Identifier
 .PARAMETER ItemId
 SharePoint List Item Identifier
 
+.PARAMETER Column
+SharePoint List Column Names
+
 .EXAMPLE
 Get-OGSiteListItem -SiteId a3299706-eac5-46a1-b5eb-5709bea18e89 -ListId 79aafc35-e805-491c-bf19-f0b6c28b6be0
 .NOTES
@@ -26,17 +29,37 @@ Function Get-OGSiteListItem {
     param (
         [Parameter(Mandatory)]$SiteId,
         [Parameter(Mandatory)]$ListId,
-        [Parameter(Mandatory = $false)]$ItemId
+        [Parameter(Mandatory = $false)]$ItemId,
+        [Parameter(Mandatory = $false)]$Column
     )
     if ($ItemId) {
-        $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items?expand=fields"
-        $response = Get-OGNextPage -uri $URI | Where-Object id -EQ $ItemId
-        $response.fields
+        if ($Column) {
+            $columnString = $column -join ','
+            $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items/$($itemId)?`$expand=fields(`$select=$ColumnString)"
+            $response = Get-OGNextPage -uri $URI
+            $response.fields
+        }
+        else {
+            $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items/$itemId"
+            $response = Get-OGNextPage -uri $URI
+            $response.fields
+        }
     }
     else {
-        $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items?expand=fields"
-        $response = Get-OGNextPage -uri $URI
-        $response.fields
+        if ($Column) {
+            $ColumnString = $Column -join ','
+            $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items?`$expand=fields(`$select=$ColumnString)"
+
+            write-host $URI
+            $response = Get-OGNextPage -uri $URI
+            $response.fields
+        }
+        else {
+            $URI = "/$GraphVersion/sites/$SiteId/lists/$ListId/items?expand=fields"
+            $response = Get-OGNextPage -uri $URI
+            $response.fields
+        }
+
     }
 
 }

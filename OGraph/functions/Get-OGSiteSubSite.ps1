df616b7d6dba3fb6e2188cf37759adb0d7b769e4
Ben Pulido <105318900+clouddots-ben@users.noreply.github.com>
2024-10-08 10:42:21 -0400

    Updated name

--- OGraph/functions/Get-OGSiteSubSite.ps1
+++ OGraph/functions/Get-OGSiteSubSite.ps1
@@ -11,12 +11,12 @@ Permissions: https://learn.microsoft.com/en-us/graph/api/site-list-subsites?view
 SharePoint Site Identifier
 
 .EXAMPLE
-Get-OGSiteSubSite -SiteId f232e745-0801-4705-beb6-4d9880fc92b4
+Get-OGSiteSubsite -SiteId f232e745-0801-4705-beb6-4d9880fc92b4
 
 .NOTES
 General notes
 #>
-Function Get-OGSiteSubSite {
+Function Get-OGSiteSubsite {
 
     [CmdletBinding(DefaultParameterSetName = 'SID')]
     Param(
